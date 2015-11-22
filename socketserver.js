ff0b610e9e79d4e95a0ef445184e06a30e4996f1
Johannes Bertens <johannes.bertens@live.nl>
2015-11-22 16:56:38 +0100

    Sending references now.

--- socketserver.js
+++ socketserver.js
@@ -3,10 +3,18 @@ var client = require('socket.io-client')('http://localhost:3210');
 var redis = require('socket.io-redis');
 var request = require('request');
 
-// IO adapter to get messages from other senders
-io.adapter(redis({ host: 'redis.core.djbnjack.svc.tutum.io', port: 6379 }));
-
+var redis_url = 'redis.core.djbnjack.svc.tutum.io:6379';
+var neo4j_url = 'http://neo4j.core.djbnjack.svc.tutum.io:8080';
+var statsd_url = 'http://statsd.core.djbnjack.svc.tutum.io:8081';
 var processes_url = "http://processes-api.api-layer.djbnjack.svc.tutum.io:3000/processes"; 
+var url_collection = {
+    'redis_url': redis_url,
+    'neo4j_url': neo4j_url,
+    'statsd_url': statsd_url
+}
+
+// IO adapter to get messages from other senders
+io.adapter(redis(redis_url));
 
 var processes_socket = io.of('/processes');
 processes_socket.on('connection', function(socket){
@@ -16,6 +24,7 @@ processes_socket.on('connection', function(socket){
 
 io.on('connection', function(socket) {
 	console.log('new system connection');
+    io.emit('urls', url_collection);
 });
 
 client.on('updated', function(msg){
