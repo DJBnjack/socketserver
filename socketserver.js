90fc9eb3211564aebbaba0315ed22774a46922ca
Johannes Bertens <johannes.bertens@live.nl>
2015-11-22 15:55:23 +0100

    Simplified socketserver

--- socketserver.js
+++ socketserver.js
@@ -6,29 +6,12 @@ var request = require('request');
 // IO adapter to get messages from other senders
 io.adapter(redis({ host: 'redis.core.djbnjack.svc.tutum.io', port: 6379 }));
 
-var processes = [];
-var updateProcesses = function(callback) {
-    var url = "http://processes-api.api-layer.djbnjack.svc.tutum.io:3000/processes";
-    // var url = "http://localhost:3000/processes";
-    request(url, function (error, response, body) {
-        if(error){
-            console.log('Error:', error);
-        } else if(response.statusCode !== 200){
-            console.log('Invalid Status Code Returned:', response.statusCode);
-        }
-    
-        processes = body;
-        if (callback != null) callback(processes);
-    });    
-}
-
-// Initial load of processes
-updateProcesses();
+var processes_url = "http://processes-api.api-layer.djbnjack.svc.tutum.io:3000/processes"; 
 
 var processes_socket = io.of('/processes');
 processes_socket.on('connection', function(socket){
 	console.log('new process connection');
-	socket.emit('processes', processes);
+	socket.emit('url', processes_url);
 });
 
 io.on('connection', function(socket) {
@@ -38,7 +21,7 @@ io.on('connection', function(socket) {
 client.on('updated', function(msg){
     console.log('updated: ' + msg);
     if (msg == 'processes') {
-        updateProcesses((updatedProcesses) => processes_socket.emit('processes', updatedProcesses));
+        processes_socket.emit('updated', 'all');
     }
 });
 
