a6ad09b7f7aab8de048c4fadd738b8bada926f60
Johannes Bertens <johannes.bertens@live.nl>
2015-11-23 22:01:25 +0100

    Removed redis url

--- socketserver.js
+++ socketserver.js
@@ -1,12 +1,10 @@
 var io = require('socket.io')(3210);
 var request = require('request');
 
-var redis_url = 'redis.core.djbnjack.svc.tutum.io:6379';
 var neo4j_url = 'http://neo4j.core.djbnjack.svc.tutum.io:8080';
 var statsd_url = 'http://statsd.core.djbnjack.svc.tutum.io:8081';
 var processes_url = "http://processes-api.api-layer.djbnjack.svc.tutum.io:3000/processes"; 
 var url_collection = {
-    'redis_url': redis_url,
     'neo4j_url': neo4j_url,
     'statsd_url': statsd_url
 }
@@ -18,7 +16,7 @@ processes_socket.on('connection', function(socket){
 });
 
 io.on('connection', function(socket) {
-	console.log('new system connection');
+	console.log('new connection');
     socket.emit('urls', url_collection);
 
     socket.on('updated', function(msg){
